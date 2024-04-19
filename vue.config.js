ca0444f44b8ed2060919f41c7057cbb0c5b80b69
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-19 16:36:17 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
     ],
 	  port:8000,
   },
-  publicPath: './',
+  publicPath: '/',
   chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       config.plugin('html').tap((opts) => {
