b64f41bd8852681d2e9bff59256ceac4d44f388d
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-20 14:05:22 -0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -500,7 +500,7 @@ export default {
     loadCollection(id, show=false){
       this.state.loadingCollections = true
       let sendData = {
-        userID: this.state.userID,
+        userID: this.state.loggedinUserID,
         passhash: this.state.passhash,
         //page: this.state.collectionsPage,
         //maxResultsPerPage: this.state.maxCollectionResultsPerPage
