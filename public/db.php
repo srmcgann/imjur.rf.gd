56b7bc8d13d094d9dcd71405441b2fb654617f1b
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-20 10:35:42 -0700

    Delete public/db.php

--- public/db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-  $db_user  = 'if0_36394385';
-  $db_pass  = 'Mr8KYrqcimRI';
-  $db_host  = 'sql308.infinityfree.com';
-  $db       = "if0_36394385_imjur";
-  $port     = '3306';
-  $link     = mysqli_connect($db_host,$db_user,$db_pass,$db,$port);
-?>
