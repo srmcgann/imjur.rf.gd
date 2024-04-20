debc0c9562f855fad6b309ad1857f37f13741513
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-20 13:31:50 -0700

    Add files via upload

---
+++ public/loadCollection.php
@@ -0,0 +1,67 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+  require_once('db.php');
+  require_once('functions.php');
+  $data = json_decode(file_get_contents('php://input'));
+  $userID = mysqli_real_escape_string($link, $data->{'userID'});
+  $passhash = mysqli_real_escape_string($link, $data->{'passhash'});
+  $collectionID = mysqli_real_escape_string($link, $data->{'collectionID'});
+
+  // uncomment for paginated collections
+
+  //$page = mysqli_real_escape_string($link, $data->{'page'});
+  //$overrideMaxResults = mysqli_real_escape_string($link, $data->{'maxResultsPerPage'});
+  //if($overrideMaxResults) $maxResultsPerPage = $overrideMaxResults;
+  //$start = $maxResultsPerPage * $page;
+
+  if($passhash){
+    $sql = "SELECT * FROM imjurUsers WHERE id = $userID AND passhash LIKE BINARY\"$passhash\"";
+    $res = mysqli_query($link, $sql);
+    $row = mysqli_fetch_assoc($res);
+    $admin = $row['admin'];
+    $enabled = $row['enabled'];
+  }
+
+  //$sql="SELECT id FROM imjurCollections WHERE id = $collectionID AND (($enabled AND userID = $userID) OR $admin OR private = 0)";
+  //$res = mysqli_query($link, $sql);
+  //$totalRecords = mysqli_num_rows($res);
+  //$totalPages = floor(($totalRecords-1) / $maxResultsPerPage) + 1;
+  $totalPages = 1
+  
+  //$sql = "SELECT * FROM imjurCollections WHERE id = $collectionID AND (($enabled AND userID = $userID) OR $admin OR private = 0) ORDER BY id ASC LIMIT $start, $maxResultsPerPage";
+  
+  $sql = "SELECT * FROM imjurCollections WHERE id = $collectionID AND (($enabled AND userID = $userID) OR $admin OR private = 0)";
+  $res = mysqli_query($link, $sql);
+  
+  $collections = [];
+  for($i=0; $i<mysqli_num_rows($res); ++$i){
+    $row = mysqli_fetch_assoc($res);
+    $private = json_decode($row['meta'])->{'private'};
+    if(!$private || ($passhash && ($admin || $enabled))){
+      $ar           = [];
+      $ar['id']     = $row['id'];
+      $ar['name']   = $row['name'];
+      $ar['userID'] = $userID;
+      $ar['meta']   = [
+                      'date'          => json_decode($row['meta'])->{'date'},
+                      'description'   => json_decode($row['meta'])->{'description'},
+                      'slugs'         => json_decode($row['meta'])->{'slugs'},
+                      'upvotes'       => json_decode($row['meta'])->{'upvotes'},
+                      'downvotes'     => json_decode($row['meta'])->{'downvotes'},
+                      'private'       => $private,
+                      'views'         => json_decode($row['meta'])->{'views'},
+                      //'originalSlugs' => json_decode($row['meta'])->{'originalSlugs'},
+                      'serverTZO'     => getServerTZOffset(),
+      ];
+      $collections[] = $ar;
+    }
+  }
+  if(sizeof($collections)){
+    echo json_encode([true, $collections[0], $totalPages]);
+  }else{
+    echo json_encode([false]);
+  }
+?>
--- src/App.vue
+++ src/App.vue
@@ -90,6 +90,7 @@ export default {
         previewLink: null,
         passhash: '',
         loggedinUserID: null,
+        loadCollection: null,
         fetchCollections: null,
         click: false,
         loadLinks: null,
@@ -104,6 +105,7 @@ export default {
         },
         loggedinUserName: '',
         copyLink: null,
+        viewCollection: null,
         deleteSingle: null,
         syncCache: null,
         deleteCollection: null,
@@ -149,6 +151,7 @@ export default {
         regpassword: '',
         showUploadModal: false,
         loadingAssets: true,
+        previewCollection: null,
         loadingCollections: true,
         checkLogin: null,
         adminData: null,
@@ -228,6 +231,12 @@ export default {
     showUserSettings(){
       document.getElementsByTagName('HTML')[0].style.overflowY = 'hidden'
       this.state.userSettingsVisible = true
+    },
+    viewCollection(collection){
+      this.state.previewCollection = collection
+      this.state.miscLinks = []
+      this.state.loadLinks(collection.meta.slugs)
+      this.state.mode = 'collection'
     },
     firstPage(){
       let search = this.state.search.string ? ('/1/' + (this.state.search.string)) : ''
@@ -812,8 +821,9 @@ export default {
     getMode(){
       let vars = window.location.pathname.split('/').filter(v=>v && ''+v != 'NaN')
       if(vars.length>0){
+        console.log('vars', vars)
         let l = location.origin.toLowerCase().indexOf('000webhostapp.com') !== -1 ? 1 : 0
-        if(1||this.state.isNumber(vars[l])){
+        if(this.state.isNumber(vars[l])){
           this.state.mode = 'default'
           let search = ''
           
@@ -840,7 +850,13 @@ export default {
             this.state.mode = 'default'
           }
         }else{
-          this.state.mode = 'non-default'
+          this.state.mode = vars[l]
+          console.log('non-default mode detected: ', this.state.mode)
+          switch(this.state.mode){
+            case 'col':
+              this.viewCollection(collection)
+            break
+          }
         }
       } else{
         if(location.href !== this.URLbase + '/1') history.pushState(null,null,this.URLbase + '/1')
@@ -1353,7 +1369,9 @@ export default {
     this.state.multipleLinks = this.multipleLinks
     this.state.setLinksOwner = this.setLinksOwner
     this.state.fetchUserLinks = this.fetchUserLinks
+    this.state.viewCollection = this.viewCollection
     this.state.deleteSelected = this.deleteSelected
+    this.state.loadCollection = this.loadCollection
     this.state.openCollection = this.openCollection
     this.state.setLinkProperty = this.setLinkProperty
     this.state.showUserSettings = this.showUserSettings
@@ -1568,8 +1586,10 @@ a{
   background: #84fd;
 }
 .viewCollectionButton, .editCollectionButton{
-  min-width: 50px;
   height: 24px;
+  line-height: 14px;
+  min-width: 85px;
+  padding: 5px;
   margin: unset;
   margin-right: 5px;
 }
