20e99fb22dfcc0b41cca06804bdb24ca837ee02d
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-20 14:41:58 -0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -237,7 +237,7 @@ export default {
       this.state.miscLinks = []
       this.state.loadLinks(collection.meta.slugs)
       this.state.mode = 'col'
-      history.pushState(null,null,this.URLbase + `/col/${collection.id}` + (this.state.curPage + 1))
+      history.pushState(null,null,this.URLbase + `/col/${collection.id}` //+ (this.state.curPage + 1))
     },
     firstPage(){
       let search = this.state.search.string ? ('/1/' + (this.state.search.string)) : ''
