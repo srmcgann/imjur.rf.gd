00c1e5b25da68f42b537733b1d5acc9f1db29caa
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-21 14:08:31 -0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1345,13 +1345,21 @@ export default {
       return ret
     },
     popupVisible(){
+    console.log(this.state.userSettingsVisible ,
+      this.state.showLoginPrompt ,
+      this.state.showCollections ,
+      this.state.editCollection.length ,
+      this.state.showCollectionTemplate ,
+      this.state.showPreview ,
+      this.state.showUploadModal ,
+      this.state.showRegister)
+      
       return this.state.userSettingsVisible ||
       this.state.showLoginPrompt ||
       this.state.showCollections ||
       this.state.editCollection.length ||
       this.state.showCollectionTemplate ||
       this.state.showPreview ||
-      this.state.showAssetPreview ||
       this.state.showUploadModal ||
       this.state.showRegister
     }
