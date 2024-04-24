9556a906f33f979c7f7ba0f7f5e06be8ddb84519
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-23 18:12:46 -0700

    Add files via upload

--- public/getStats.php
+++ public/getStats.php
@@ -14,8 +14,25 @@ error_reporting(E_ALL);
     $sql = "SELECT * FROM immurUploads WHERE userID = $userID";
     $res = mysqli_query($link, $sql);
     $ret = [];
-    $ret['assets count'] = mysqli_num_rows($res));
-    echo json_encode([$success, $ret]);
+    $assets = [];
+    for($i=0; $i<mysqli_num_rows($res); ++$i){
+      $row = mysqli_fetch_assoc($res);
+      $obj = [
+        'id' => $row['id'],
+        'slug' => $row['slug'],
+        'views' => $row['views'],
+        'originalSlug' => $row['originalSlug'],
+        'date' => $row['date'],
+        'hash' => $row['hash'],
+        'upvotes' => $row['upvotes'],
+        'downvotes' => $row['downvotes'],
+        'filetype' => $row['filetype'],
+        'private' => $row['private'],
+        'meta' => $row['meta'],
+      ];
+      $assets[] = $obj;
+    }
+    echo json_encode([$success, $assets]);
   }else{
     echo json_encode([$success]);
   }
