4b093f8bb2188ea9a8ebedfd4171b0a0e20cdcab
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-23 18:01:56 -0700

    Add files via upload

--- public/getStats.php
+++ public/getStats.php
@@ -7,52 +7,16 @@ error_reporting(E_ALL);
   $data = json_decode(file_get_contents('php://input'));
   $userID = mysqli_real_escape_string($link, $data->{'userID'});
 
-  $page = mysqli_real_escape_string($link, $data->{'page'});
-  $overrideMaxResults = mysqli_real_escape_string($link, $data->{'maxResultsPerPage'});
-  if($overrideMaxResults) $maxResultsPerPage = $overrideMaxResults;
-  $start = $maxResultsPerPage * $page;
+  $success = false;
 
-  $sql="SELECT id FROM imjurUploads WHERE userID = $userID";
-  $res = mysqli_query($link, $sql);
-  $totalRecords = mysqli_num_rows($res);
-  $totalPages = floor(($totalRecords-1) / $maxResultsPerPage) + 1;
-
-  $sql = "SELECT * FROM imjurUploads WHERE userID = $userID ORDER BY date DESC LIMIT $start, $maxResultsPerPage";
-  $res = mysqli_query($link, $sql);
-  
-  $uploadDir = 'uploads';
-  $links = [];
-  $meta = [];
-  for($i=0; $i<mysqli_num_rows($res); ++$i){
-    $row = mysqli_fetch_assoc($res);
-    $slug = $row['slug'];
-    $m = [
-      'id'             => $row['id'],
-      'slug'           => $slug,
-      'size'           => json_decode($row['meta'])->{'file size'},
-      'hash'           => $row['hash'],
-      'name'           => $row['name'],
-      'filetype'       => $row['filetype'],
-      'date'           => $row['date'],
-      'userID'         => $row['userID'],
-      'origin'         => $row['origin'],
-      'upvotes'        => $row['upvotes'],
-      'private'        => $row['private'],
-      'downvotes'      => $row['downvotes'],
-      'views'          => $row['views'],
-      'description'    => $row['description'],
-      'originalSlug'   => $row['originalSlug'],
-      'originalDate'   => $row['originalDate'],
-      'serverTZO'      => getServerTZOffset(),
-    ];
-    $suffix = getSuffix($row['filetype']);
-    $originalSlug = $row['originalSlug'];
-    $links[] = "$uploadDir/$slug.$suffix";
-    $meta[] = $m;
-  }
-  if(sizeof($links)){
-    echo json_encode([true, $links, $meta, $totalPages]);
+  if($userID){
+    $success = true;
+    $sql = "SELECT * FROM immurUploads WHERE userID = $userID";
+    $res = mysqli_query($link, $sql);
+    $ret = [];
+    $ret['assets count'] = mysqli_num_rows($res));
+    echo json_encode([$success, $ret]);
   }else{
-    echo json_encode([false]);
+    echo json_encode([$success]);
   }
 ?>
