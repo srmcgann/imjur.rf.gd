26cded0e1754711c25ae0ed86428aebeddf29608
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-27 14:22:37 -0700

    Add files via upload

--- public/delete.php
+++ public/delete.php
@@ -1,9 +1,8 @@
 <?php
-////ini_set('display_errors', 1);
-////ini_set('display_startup_errors', 1);
-//error_reporting(0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
   require('db.php');
-error_reporting(0);
   $data = json_decode(file_get_contents('php://input'));
   $userName = mysqli_real_escape_string($link, $data->{'userName'});
   $passhash = mysqli_real_escape_string($link, $data->{'passhash'});
