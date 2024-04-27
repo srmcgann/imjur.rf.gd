7f08c7e20fbb44211e88ba07b54414639cdb3917
Scott R McGann <s.r.mcgann@hotmail.com>
2024-04-27 12:11:40 -0700

    Add files via upload

--- public/setUserPref.php
+++ public/setUserPref.php
@@ -9,7 +9,7 @@ error_reporting(0);
   $passhash = mysqli_real_escape_string($link, $data->{'passhash'});
   $property = mysqli_real_escape_string($link, $data->{'property'});
   $value = mysqli_real_escape_string($link, $data->{'value'});
-  $sql = 'SELECT * FROM imjurUsers WHERE id = $userID AND passhash = "'.$passhash.'"';
+  $sql = "SELECT * FROM imjurUsers WHERE id = $userID AND passhash LIKE BINARY \"$passhash\"";
   $res = mysqli_query($link, $sql);
   $success = false;
   
@@ -17,8 +17,7 @@ error_reporting(0);
   
   if(mysqli_num_rows($res)){
     $row = mysqli_fetch_assoc($res);
-    $userID = $row['id'];
-    if($row['enabled']){
+    if(!!$row['enabled']){
       $sql = "UPDATE imjurUsers SET `$property` = \"$value\" WHERE id = $userID";
       mysqli_query($link, $sql);
       $success = true;
