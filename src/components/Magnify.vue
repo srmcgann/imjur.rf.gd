aac87cee6dfa9a72540b314a182e455f373e0600
Scott R McGann <s.r.mcgann@hotmail.com>
2024-05-06 15:07:21 -0700

    Add files via upload

--- src/components/Magnify.vue
+++ src/components/Magnify.vue
@@ -49,8 +49,8 @@ export default {
         this.my = e.pageY
         this.$refs.magnifyingGlass.style.left = this.mx-200 + 'px'
         this.$refs.magnifyingGlass.style.top = this.my-200 + 'px'
-        this.contents.style.marginLeft = ((-this.mx+document.body.clientWidth/2+38)*(this.magLevel+1)-document.body.clientWidth/2) +'px'
-        this.contents.style.marginTop = (-this.my*(this.magLevel+1)+document.body.clientHeight/2*(this.magLevel+1)+38*(this.magLevel+1)*2) + 'px'
+        this.contents.style.marginLeft = ((-this.mx+document.body.clientWidth/2+38)*(this.magLevel+1)-this.element.clientWidth/2) +'px'
+        this.contents.style.marginTop = (-this.my*(this.magLevel+1)+this.element/2*(this.magLevel+1)+38*(this.magLevel+1)*2) + 'px'
       }
     }
   },
