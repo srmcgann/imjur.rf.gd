3bd91dc39ef1a0bc7183c2db04141c327f16d1c1
Scott R McGann <s.r.mcgann@hotmail.com>
2024-05-07 18:24:31 -0700

    Add files via upload

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -235,7 +235,7 @@ export default {
     this.mounted = true
   },
   beforeUnmount(){
-    if(this.linkType == 'video' || this.linkType == 'audio') this.asset.paused = true
+    if(typeof this.asset?.paused == 'boolean') this.asset.paused = true
     this.asset.src = ''
   }
 }
