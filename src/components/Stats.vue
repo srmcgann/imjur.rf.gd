85d41353dfb3620ec9e1c7efc8a84d7e0b6c8d5c
Scott R McGann <s.r.mcgann@hotmail.com>
2024-05-09 13:08:44 -0700

    Add files via upload

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -9,7 +9,7 @@
   </button>
   <div class="stats" ref="stats" tabindex="1000">
     <div class="modalInner" style="overflow: auto;">
-      overview of<br><b>{{state.loggedinUserName}}'s</b> assets<br>
+      overview of<br><b>{{state.userInfo[state.userStatsID].name}}'s</b> assets<br>
       <table class="statsTable">
         <tr>
           <td
