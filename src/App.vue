aa5b5e624d4119e0ed46366ee14f420af194db82
Scott R McGann <s.r.mcgann@hotmail.com>
2024-05-13 12:42:17 -0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -677,7 +677,7 @@ export default {
         body: JSON.stringify(sendData),
       }).then(res => res.json()).then(data=>{
         if(data[0]){
-          state.featuredItems = data[1]
+          this.state.featuredItems = data[1]
         }else{
           alert('there was an error loading featured items')
         }
